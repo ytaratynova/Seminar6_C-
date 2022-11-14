f10374301b247a07883e3acee60b1d37f0e17345
Iuliia <ytaratynova@gmail.com>
2022-11-14 15:27:25 +0300

    homework seminar6

---
+++ Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int size) // просим пользователя ввести числа и записываем их в массив
+{
+    int[] arr = new int[size];
+    Console.WriteLine($"Введите {size} целых чисел: ");
+    for(int i = 0; i < arr.Length; i ++)
+    {
+        Console.WriteLine($"число {i+1}: ");
+        arr[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+    return arr;
+}
+
+int CountPositive(int[] array) // считаем количество чисел в массиве, которые > 0
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if (element > 0) count ++;
+    }
+    return count;
+}
+
+int m = new Random().Next(1, 10); // случайным образом выбираем количество чисел, которое должен ввести пользователь
+int[] userMassiv = FillArray(m);
+Console.WriteLine($"Количество позитивных чисел, введенных вами: {CountPositive(userMassiv)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("введите коэффициент b1:");
+double b1 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("введите коэффициент k1:");
+double k1 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("введите коэффициент b2:");
+double b2 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("введите коэффициент k2:");
+double k2 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Точка пересечения прямых: ({Xcoordinate(b1, k1, b2, k2)}; {Ycoordinate(b1, k1, b2, k2)})");
+
+double Xcoordinate(double coorb1, double coork1, double coorb2, double coork2)       
+{
+    double x = (coorb2 - coorb1)/(coork1 - coork2);
+    return x;
+}
+
+double Ycoordinate(double coorb1, double coork1, double coorb2, double coork2)     
+{
+    double y = coork1 * (coorb2 - coorb1)/(coork1 - coork2) + coorb1;
+    return y;
+}
+
+
+
+
+       
